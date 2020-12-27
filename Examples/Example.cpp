c3983741b0a746f886ebe98e5163c19a53d6265b
zhurongjun <211103376@qq.com>
2020-12-27 22:14:49 +0800

    Example

---
+++ Examples/Example.cpp
@@ -0,0 +1,137 @@
+﻿#include "Example.h"
+#include "ImguiWrap/ImguiUEWrap.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Services/ImguiGlobalContextService.h"
+#include "Widgets/Docking/SDockTab.h"
+
+AImguiExampleActor::AImguiExampleActor()
+{
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+}
+
+void AImguiExampleActor::OpenImguiDemoGlobal()
+{
+	UImguiGlobalContextService::Get().AddGlobalWindow(TEXT("Dear ImGui Demo"), FDrawGlobalImgui::CreateLambda([]
+        {
+            bool IsOpen = true;
+            ImGui::ShowDemoWindow(&IsOpen);
+            return IsOpen;
+        }));
+}
+
+void AImguiExampleActor::OpenImguiDemoDockGlobal()
+{
+	// create an global imgui widget
+	auto GlobalImguiWidget = SNew(SGlobalImguiWidget)
+    .HSizingRule(EImguiSizingRule::UESize)
+    .VSizingRule(EImguiSizingRule::UESize)
+    .WndName(TEXT("Dear ImGui Demo"))
+    .OnDraw(FOnImguiDraw::CreateLambda([]
+    {
+        ImGui::ShowDemoWindow();
+    }));
+
+	// create a dock tab 
+	TSharedPtr<SDockTab> NewTab = SNew(SDockTab)
+    .Label(FText::FromString(TEXT("Dear ImGui Demo")))
+    .TabRole(ETabRole::NomadTab)
+    [
+		GlobalImguiWidget
+    ];
+
+	// register dock tab  	
+	static FName PlaceholderId(TEXT("StandaloneToolkit"));
+	static TSharedPtr<FTabManager::FSearchPreference> SearchPreference = MakeShareable(new FTabManager::FRequireClosedTab());
+	FGlobalTabmanager::Get()->InsertNewDocumentTab(PlaceholderId, *SearchPreference, NewTab.ToSharedRef());
+}
+
+void AImguiExampleActor::OpenImguiDemoStandAlone()
+{
+	// create an stand alone imgui widget
+	auto ImguiWidget = SNew(SImguiWidget)
+	.OnDraw(FOnImguiDraw::CreateLambda([]
+	{
+		ImGui::ShowDemoWindow();
+	}));
+
+	// create a window
+	auto NewWnd = SNew(SWindow)
+	.Title(FText::FromString(TEXT("StandAloneImguiWindow")))
+    .SizingRule(ESizingRule::UserSized)
+	[
+		ImguiWidget
+	];
+
+	// open window
+	FSlateApplication::Get().AddWindow(NewWnd);
+}
+
+void AImguiExampleActor::OnGUI()
+{
+	ImGui::BeginDetail();
+	ImGui::Columns(2);
+
+	// simple functional 
+	ImGui::AlignTextToFramePadding();
+	ImGui::Text("Simple functional: ");
+	ImGui::NextColumn();
+	if (ImGui::Button("SayHello"))
+	{
+		UKismetSystemLibrary::PrintString(GWorld);
+	}
+	ImGui::SameLine();
+	if (ImGui::Button("Print Location"))
+	{
+		UKismetSystemLibrary::PrintString(GWorld, GetActorLocation().ToString());		
+	}
+	ImGui::NextColumn();
+
+	// global context 
+	ImGui::AlignTextToFramePadding();
+	ImGui::Text("Global context: ");
+	ImGui::NextColumn();
+	if (ImGui::Button("Open Demo"))
+	{
+		OpenImguiDemoGlobal();
+	}
+	ImGui::SameLine();
+	if (ImGui::Button("Open Demo Dock"))
+	{
+		OpenImguiDemoDockGlobal();
+	}
+	ImGui::NextColumn();
+
+	// stand alone context 
+	ImGui::AlignTextToFramePadding();
+	ImGui::Text("Stand alone context: ");
+	ImGui::NextColumn();
+	if (ImGui::Button("Open Demo Window"))
+	{
+		OpenImguiDemoStandAlone();
+	}
+	ImGui::NextColumn();
+
+	// resume 
+	ImGui::Columns(1);
+	ImGui::EndDetail();
+}
+
+void UImguiDetailCustomizationExample::OnEditSingleObject(UObject* InObject)
+{
+	ImGui::BeginDetail();
+	ImGui::Separator();
+	ImGui::Text("This is single object editor");
+	ImGui::EndDetail();
+}
+
+void UImguiDetailCustomizationExample::OnEditMultiObject(TArray<UObject*> InObjects)
+{
+	ImGui::BeginDetail();
+	ImGui::Text("Edit multi object here");
+	ImGui::Separator();
+	for (UObject* Object : InObjects)
+	{
+		ImGui::Text(Object->GetName());
+	}
+	ImGui::EndDetail();
+}
---
+++ Examples/Example.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+
+#include "Services/ImguiDetailCustomization.h"
+
+#include "Example.generated.h"
+
+UCLASS()
+class AImguiExampleActor : public AActor
+{
+	GENERATED_BODY()
+public:
+	AImguiExampleActor();
+
+	// open imgui demo window(use global context)
+	void OpenImguiDemoGlobal();
+	
+	// open imgui demo dock window(use global context)
+	void OpenImguiDemoDockGlobal();
+	
+	// open imgui demo window(use stand alone context)
+	void OpenImguiDemoStandAlone();
+	
+	// use OnGUI function for customize detail panel 
+	UFUNCTION()
+	void OnGUI();
+
+protected:
+	UPROPERTY(EditAnywhere)
+	USceneComponent* Root;
+};
+
+UCLASS()
+class UImguiDetailCustomizationExample : public UImguiDetailCustomization
+{
+	GENERATED_BODY()
+public:
+	// target class 
+	virtual UClass* GetSupportClass() override { return AActor::StaticClass(); }
+
+	// support single object editing 
+	virtual bool SupportSingleObjectEditing() override { return true; }
+	virtual void OnEditSingleObject(UObject* InObject) override;
+
+	// support mult object editing 
+	virtual bool SupportMultiObjectEditing() override { return true; }
+	virtual void OnEditMultiObject(TArray<UObject*> InObjects) override;
+};
+
+
