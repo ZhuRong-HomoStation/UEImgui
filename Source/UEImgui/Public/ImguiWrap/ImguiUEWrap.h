92e3a5e79e8805b3863e349dd5f9f22ed2316eeb
zhurongjun <211103376@qq.com>
2021-01-10 12:47:40 +0800

    子曰: "非礼勿视"

--- Source/UEImgui/Public/ImguiWrap/ImguiUEWrap.h
+++ Source/UEImgui/Public/ImguiWrap/ImguiUEWrap.h
@@ -8,29 +8,34 @@ class SImguiWidgetRenderProxy;
 
 enum ImguiWindowFlagUE
 {
-	ImGuiWindowFlags_UEDetail = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoScrollbar
+	ImGuiWindowFlags_UEDetail = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_AlwaysAutoResize 
 };
 
 // Unreal draw functions 
 namespace ImGui
 {
+	// =============Unreal Style============= 
 	UEIMGUI_API void StyleColorUE(ImGuiStyle* dst = nullptr);		// Unreal style 
 	UEIMGUI_API void StyleColorConfig(ImGuiStyle* dst = nullptr);	// Config style
 	UEIMGUI_API bool ShowUEStyleSelector(const char* Label);		// Unreal style editor 
 	UEIMGUI_API void ShowUEStyleEditor();							// Unreal style editor 
 
+	// =============Detail=============
 	UEIMGUI_API void SetCurrentDetail(FName InDetailName);			// internal: used by detail customization 
 	UEIMGUI_API void SetCurrentDetailWidget(TWeakPtr<SImguiWidgetRenderProxy> InDetailWidget);	// internal: used by detail customization
-	UEIMGUI_API FName GetCurrentDetail();	// Get current detail name 
-	UEIMGUI_API void BeginDetail();		// Begin detail for detail customization 
-	UEIMGUI_API void EndDetail();		// End detail for detail customization
+	UEIMGUI_API FName GetCurrentDetail();		// Get current detail name 
+	UEIMGUI_API void BeginDetail();				// Begin detail for detail customization 
+	UEIMGUI_API void EndDetail();				// End detail for detail customization
 
+	// =============IME=============
 	UEIMGUI_API void SetCurrentWidget(TWeakPtr<SWidget> InWidget);	// internal: setup widget that render current imgui 
-	UEIMGUI_API TWeakPtr<SWidget> GetCurrentWidget();	// internal: get widget that render current imgui
+	UEIMGUI_API TWeakPtr<SWidget> GetCurrentWidget();				// internal: get widget that render current imgui
+	UEIMGUI_API void UseInputSystem(const char* label, ImGuiInputTextFlags flags = 0);
 
+	// =============Text=============
 	UEIMGUI_API void Text(const FString& InString);
 
-	UEIMGUI_API void UseInputSystem(const char* label, ImGuiInputTextFlags flags = 0);
+	// =============InputText=============
 	UEIMGUI_API bool InputText(const char* label, std::string* str, ImGuiInputTextFlags flags = 0, ImGuiInputTextCallback callback = NULL, void* user_data = NULL);
 	UEIMGUI_API bool InputTextMultiline(const char* label, std::string* str, const ImVec2& size = ImVec2(0, 0), ImGuiInputTextFlags flags = 0, ImGuiInputTextCallback callback = NULL, void* user_data = NULL);
 	UEIMGUI_API bool InputTextWithHint(const char* label, const char* hint, std::string* str, ImGuiInputTextFlags flags = 0, ImGuiInputTextCallback callback = NULL, void* user_data = NULL);
