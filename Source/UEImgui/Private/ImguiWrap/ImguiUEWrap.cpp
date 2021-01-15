e100372e07829aa20ec23f97ba80309038a42bc4
zhurongjun <211103376@qq.com>
2021-01-15 18:09:24 +0800

    Simple Detail上线！！！

--- Source/UEImgui/Private/ImguiWrap/ImguiUEWrap.cpp
+++ Source/UEImgui/Private/ImguiWrap/ImguiUEWrap.cpp
@@ -23,19 +23,6 @@ static void _HelpMarker(const char* desc)
 
 using ImPropertyDisplayFunction = bool(*)(FProperty*, void*);
 
-FORCEINLINE ImPropertyDisplayFunction _GetDisplayFunction(const FString& InCppType)
-{
-	static TMap<FString, ImPropertyDisplayFunction> FunctionMap;
-
-	// init function map
-	if (FunctionMap.Num() == 0)
-	{
-		
-	}
-
-	return FunctionMap[InCppType];
-}
-
 // detail
 FName CurrentDetail;
 TWeakPtr<SImguiRenderProxy> CurrentDetailWidget;
@@ -246,12 +233,16 @@ bool ImGui::UEEnum(const char* InLabel, UEnum* InEnumClass, int64* EnumSource)
 
 bool ImGui::UEStruct(UScriptStruct* InStruct, void* InValue)
 {
-	FString CppType;
-	for (TFieldIterator<FProperty> It(InStruct); It; ++It)
+	bool bHasChanged = false;
+	if (ImGui::BeginTable("ShitSHitSHit", 2, ImGuiTableFlags_Resizable | ImGuiTableFlags_BordersInnerV | ImGuiTableFlags_SizingPolicyStretch))
 	{
-		UEProperty(*It, InValue);
+		for (TFieldIterator<FProperty> It(InStruct); It; ++It)
+		{
+			bHasChanged |= UEProperty(*It, InValue);
+		}
+		ImGui::EndTable();
 	}
-	return true;
+	return bHasChanged;
 }
 
 bool ImGui::UEObject(UClass* InClass, void* InValue)
@@ -261,31 +252,233 @@ bool ImGui::UEObject(UClass* InClass, void* InValue)
 
 bool ImGui::UEProperty(FProperty* InProperty, void* InContainer)
 {
-	void* PropertyData = InProperty->ContainerPtrToValuePtr<void>(InContainer);
+	// constant
+	const char  s8_min  = -128,			s8_max = 127;
+	const ImU8  u8_min  = 0,			u8_max = 255;
+	const short s16_min = -32768,		s16_max = 32767;
+	const ImU16 u16_min = 0,			u16_max = 65535;
+	const ImS32 s32_min = INT_MIN,		s32_max = INT_MAX;
+	const ImU32 u32_min = 0,			u32_max = UINT_MAX;
+	const ImS64 s64_min = LLONG_MIN,	s64_max = LLONG_MAX;
+	const ImU64 u64_min = 0,			u64_max = ULLONG_MAX;
 
-	if (FStructProperty* StructProperty = CastField<FStructProperty>(InProperty))
+	// string buffer
+	static std::string StrBuffer;
+	
+	// get data 
+	void* PropertyData = InProperty->ContainerPtrToValuePtr<void>(InContainer);
+	bool bValueHasChanged = false;
+	
+	// draw label
+	std::string PropertyName = TCHAR_TO_UTF8(*InProperty->GetName());
+	ImGui::TableNextRow();
+	ImGui::TableSetColumnIndex(0);
+	ImGui::AlignTextToFramePadding();
+	ImGui::Text(PropertyName.c_str());
+	ImGui::TableSetColumnIndex(1);
+
+	// setup label
+	PropertyName = "##" + PropertyName;
+	
+	// draw value 
+	switch ((EClassCastFlags)InProperty->GetClass()->GetId())
 	{
-		return UEStruct(StructProperty->Struct, PropertyData);
+	// ========================Integer========================
+	case CASTCLASS_FInt8Property:
+		bValueHasChanged = ImGui::DragScalar(PropertyName.c_str(), ImGuiDataType_S8, PropertyData, 1.f, &s8_min, &s8_max);
+		break;
+	case CASTCLASS_FByteProperty:
+		bValueHasChanged = ImGui::DragScalar(PropertyName.c_str(), ImGuiDataType_U8, PropertyData, 1.f, &u8_min, &u8_max);
+		break;
+	case CASTCLASS_FInt16Property:
+		bValueHasChanged = ImGui::DragScalar(PropertyName.c_str(), ImGuiDataType_S16, PropertyData, 1.f, &s16_min, &s16_max);
+		break;
+	case CASTCLASS_FUInt16Property:
+		bValueHasChanged = ImGui::DragScalar(PropertyName.c_str(), ImGuiDataType_U16, PropertyData, 1.f, &u16_min, &u16_max);
+		break;
+	case CASTCLASS_FIntProperty:
+		bValueHasChanged = ImGui::DragScalar(PropertyName.c_str(), ImGuiDataType_S32, PropertyData, 1.f, &s32_min, &s32_max);
+		break;
+	case CASTCLASS_FUInt32Property:
+		bValueHasChanged = ImGui::DragScalar(PropertyName.c_str(), ImGuiDataType_U32, PropertyData, 1.f, &u32_min, &u32_max);
+		break;
+	case CASTCLASS_FInt64Property:
+		bValueHasChanged = ImGui::DragScalar(PropertyName.c_str(), ImGuiDataType_S64, PropertyData, 1.f, &s64_min, &s64_max);
+		break;
+	case CASTCLASS_FUInt64Property:
+        bValueHasChanged = ImGui::DragScalar(PropertyName.c_str(), ImGuiDataType_U64, PropertyData, 1.f, &u64_min, &u64_max);
+		break;
+
+	// ========================Floating========================
+	case CASTCLASS_FFloatProperty:
+		bValueHasChanged = ImGui::DragScalar(PropertyName.c_str(), ImGuiDataType_Float, PropertyData, 1.f);
+		break;
+	case CASTCLASS_FDoubleProperty:
+		bValueHasChanged = ImGui::DragScalar(PropertyName.c_str(), ImGuiDataType_Double, PropertyData, 1.f);
+		break;
+
+	// ========================Boolean========================		
+	case CASTCLASS_FBoolProperty:
+		bValueHasChanged = ImGui::Checkbox(PropertyName.c_str(), (bool*)PropertyData);
+		break;
+		
+	// ========================String========================
+	case CASTCLASS_FNameProperty:
+	{
+		FName& NameValue = *(FName*)PropertyData;
+		FString NameStr = NameValue.ToString();
+		int32 UTF8Len = FTCHARToUTF8_Convert::ConvertedLength(*NameStr, NameStr.Len());
+		StrBuffer.resize(UTF8Len + 1);
+		StrBuffer.back() = 0;
+		FTCHARToUTF8_Convert::Convert(const_cast<char*>(StrBuffer.data()), StrBuffer.length() + 1, *NameStr, NameStr.Len());
+		bValueHasChanged = ImGui::InputText(PropertyName.c_str(), &StrBuffer);
+		if (bValueHasChanged)
+		{
+			NameValue = FName(UTF8_TO_TCHAR(StrBuffer.c_str()));
+		}
+		break;
 	}
-	else if (FObjectProperty* ObjectProperty = CastField<FObjectProperty>(InProperty))
+	case CASTCLASS_FTextProperty:
 	{
-		return UEObject(ObjectProperty->PropertyClass, ObjectProperty->GetObjectPropertyValue(InContainer));
+		FText& TextValue = *(FText*)PropertyData;
+		FString NameStr = TextValue.ToString();
+		int32 UTF8Len = FTCHARToUTF8_Convert::ConvertedLength(*NameStr, NameStr.Len());
+		StrBuffer.resize(UTF8Len + 1);
+		StrBuffer.back() = 0;
+		FTCHARToUTF8_Convert::Convert(const_cast<char*>(StrBuffer.data()), StrBuffer.length() + 1, *NameStr, NameStr.Len());
+		bValueHasChanged = ImGui::InputText(PropertyName.c_str(), &StrBuffer);
+		if (bValueHasChanged)
+		{
+			int32 TCHARLen = FUTF8ToTCHAR_Convert::ConvertedLength(StrBuffer.c_str(), StrBuffer.length());
+			NameStr.GetCharArray().SetNumUninitialized(TCHARLen + 1);
+			NameStr.GetCharArray().Top() = 0;
+			FUTF8ToTCHAR_Convert::Convert(NameStr.GetCharArray().GetData(), NameStr.GetCharArray().Num(), StrBuffer.c_str(), StrBuffer.length());
+			TextValue = FText::FromString(NameStr);
+		}
+		break;
 	}
-	else if (FEnumProperty* EnumProperty = CastField<FEnumProperty>(InProperty))
+	case CASTCLASS_FStrProperty:
 	{
+		FString& NameStr = *(FString*)PropertyData;
+		int32 UTF8Len = FTCHARToUTF8_Convert::ConvertedLength(*NameStr, NameStr.Len());
+		StrBuffer.resize(UTF8Len + 1);
+		StrBuffer.back() = 0;
+		FTCHARToUTF8_Convert::Convert(const_cast<char*>(StrBuffer.data()), StrBuffer.length() + 1, *NameStr, NameStr.Len());
+		bValueHasChanged = ImGui::InputText(PropertyName.c_str(), &StrBuffer);
+		if (bValueHasChanged)
+		{
+			int32 TCHARLen = FUTF8ToTCHAR_Convert::ConvertedLength(StrBuffer.c_str(), StrBuffer.length());
+			NameStr.GetCharArray().SetNumUninitialized(TCHARLen + 1);
+			NameStr.GetCharArray().Top() = 0;
+			FUTF8ToTCHAR_Convert::Convert(NameStr.GetCharArray().GetData(), NameStr.GetCharArray().Num(), StrBuffer.c_str(), StrBuffer.length());
+		}
+		break;
+	}
+
+	// ========================Object========================
+	case CASTCLASS_FObjectProperty:
+	case CASTCLASS_FWeakObjectProperty:
+	case CASTCLASS_FLazyObjectProperty:
+	case CASTCLASS_FSoftObjectProperty:
+		break;
+
+	// ========================Class========================
+	case CASTCLASS_FClassProperty:
+	case CASTCLASS_FSoftClassProperty:
+		break;
+		
+	// ========================Combo========================
+	case CASTCLASS_FEnumProperty:
+	{
+		FEnumProperty* EnumProperty = CastField<FEnumProperty>(InProperty);
 		EnumProperty->GetClass()->GetId();
 		FNumericProperty* LocalUnderlyingProp = EnumProperty->GetUnderlyingProperty();
 		int64 Value = LocalUnderlyingProp->GetSignedIntPropertyValue(PropertyData);
-		bool bHasChanged = UEEnum(TCHAR_TO_UTF8(*EnumProperty->GetName()), EnumProperty->GetEnum(), &Value);
+		bValueHasChanged = UEEnum(PropertyName.c_str(), EnumProperty->GetEnum(), &Value);
 		LocalUnderlyingProp->SetIntPropertyValue(PropertyData, Value);
-		return bHasChanged;
+		break;
 	}
-	else
+	case CASTCLASS_FStructProperty:
 	{
-		FString CppType = InProperty->GetCPPType();
-		ImPropertyDisplayFunction DisplayFunction = _GetDisplayFunction(CppType);
-		return DisplayFunction(InProperty, PropertyData);
+		FStructProperty* StructProperty = CastField<FStructProperty>(InProperty);
+		bValueHasChanged = UEStruct(StructProperty->Struct, PropertyData);
+		break;
 	}
+
+	// ========================Misc======================== 
+	case CASTCLASS_FArrayProperty:
+	case CASTCLASS_FMapProperty:
+	case CASTCLASS_FSetProperty:
+		break;
+
+	// ========================AActor========================
+	case CASTCLASS_AActor:
+	case CASTCLASS_APlayerController:
+	case CASTCLASS_APawn:
+		break;
+
+	// ========================Reflection[Unused]========================
+	case CASTCLASS_UField:
+	case CASTCLASS_UStruct:
+	case CASTCLASS_UScriptStruct:
+	case CASTCLASS_UClass:
+	case CASTCLASS_UEnum:
+	case CASTCLASS_UFunction:
+		
+	// ========================BasicProperty[Unused]========================
+	case CASTCLASS_None:
+	case CASTCLASS_FProperty:
+	case CASTCLASS_FInterfaceProperty:
+	case CASTCLASS_FNumericProperty:
+	case CASTCLASS_FObjectPropertyBase:
+	case CASTCLASS_FFieldPathProperty:
+
+	// ========================Delegate[Unused]========================
+	case CASTCLASS_FDelegateProperty:
+	case CASTCLASS_FMulticastDelegateProperty:
+	case CASTCLASS_FMulticastInlineDelegateProperty:
+	case CASTCLASS_FMulticastSparseDelegateProperty:
+
+	// ========================EngineMisc[Unused]========================
+	case CASTCLASS_UPackage:
+	case CASTCLASS_ULevel:
+	case CASTCLASS_USceneComponent:
+	case CASTCLASS_UPrimitiveComponent:
+	case CASTCLASS_USkinnedMeshComponent:
+	case CASTCLASS_USkeletalMeshComponent:
+	case CASTCLASS_UBlueprint:
+	case CASTCLASS_UDelegateFunction:
+	case CASTCLASS_UStaticMeshComponent:
+	case CASTCLASS_USparseDelegateFunction:
+	default:
+		ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1,0,0,1));
+		ImGui::Text("UnSupport");
+		ImGui::PopStyleColor();
+		break;
+	}
+	
+	return bValueHasChanged;
+	// if (FStructProperty* StructProperty = CastField<FStructProperty>(InProperty))
+	// {
+	// 	return UEStruct(StructProperty->Struct, PropertyData);
+	// }
+	// else if (FObjectProperty* ObjectProperty = CastField<FObjectProperty>(InProperty))
+	// {
+	// 	return UEObject(ObjectProperty->PropertyClass, ObjectProperty->GetObjectPropertyValue(InContainer));
+	// }
+	// else if (FEnumProperty* EnumProperty = CastField<FEnumProperty>(InProperty))
+	// {
+	// 	EnumProperty->GetClass()->GetId();
+	// 	FNumericProperty* LocalUnderlyingProp = EnumProperty->GetUnderlyingProperty();
+	// 	int64 Value = LocalUnderlyingProp->GetSignedIntPropertyValue(PropertyData);
+	// 	bool bHasChanged = UEEnum(TCHAR_TO_UTF8(*EnumProperty->GetName()), EnumProperty->GetEnum(), &Value);
+	// 	LocalUnderlyingProp->SetIntPropertyValue(PropertyData, Value);
+	// 	return bHasChanged;
+	// }
+	// else
+	// {
+	// 	return false;
+	// }
 }
 
 bool ImGui::ShowUEStyleSelector(const char* Label)
