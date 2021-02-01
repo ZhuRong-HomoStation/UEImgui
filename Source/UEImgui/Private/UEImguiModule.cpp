d247283439aa1fa52549d8fd391020cb752a8bc9
zhurongjun <211103376@qq.com>
2021-02-01 15:15:44 +0800

    修复4.26Demo崩溃

--- Source/UEImgui/Private/UEImguiModule.cpp
+++ Source/UEImgui/Private/UEImguiModule.cpp
@@ -1,18 +1,22 @@
 #include "Interfaces/IPluginManager.h"
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
+#include "Services/ImguiGlobalContextService.h"
 
 #define LOCTEXT_NAMESPACE "FUEImguiModule"
 
 class UEIMGUI_API FUEImguiModule : public IModuleInterface
 {
-	public:
+public:
 	// ~Begin IModuleInterface API 
 	virtual void StartupModule() override
 	{
 		// register shader dictionary 
 		FString ShaderDir = FPaths::Combine(IPluginManager::Get().FindPlugin(TEXT("UEImgui"))->GetBaseDir(),TEXT("Shaders"));
 		AddShaderSourceDirectoryMapping(TEXT("/Plugin/UEImgui"), ShaderDir);
+
+		// init call 
+		UEImGui::TimeToDraw();
 	}
 	virtual void ShutdownModule() override
 	{
